715ef70619e480ac535249e64625eb76aa5c4ddd
hartator <hartator@gmail.com>
2019-01-28 14:57:06 -0600

    Fix tests

--- spec/knowledge_graph_mcdonalds_spec.rb
+++ spec/knowledge_graph_mcdonalds_spec.rb
@@ -2,18 +2,11 @@ require 'cgi'
 
 describe "SerpApi Desktop JSON" do
 
-  before(:all) do
-    @host = 'https://serpapi.com'
-    if ENV['SERPAPI_MODE'] == 'dev'
-       @host = 'http://localhost:3000'
-    end
- end
-
  # TODO test people_also_search_for_link
  describe "Knowledge Graph for MacDonald's" do
 
     before :all do
-      @response = HTTP.get @host + '/search.json?q=McDonald%27s&location=Dallas&hl=en&gl=us&source=test'
+      @response = HTTP.get 'https://serpapi.com/search.json?q=McDonald%27s&location=Dallas&hl=en&gl=us&source=test'
       @json = @response.parse
     end
 
@@ -58,7 +51,7 @@ describe "SerpApi Desktop JSON" do
       before :all do
         people_also_search_for_link = @json["knowledge_graph"]["people_also_search_for_link"]
         stick_parameter = CGI.parse(URI.parse(people_also_search_for_link).query)["stick"][0]
-        @response_stick = HTTP.get "#{@host}/search.json?q=McDonald%27s&location=Dallas&hl=en&gl=us&source=test&stick=#{stick_parameter}"
+        @response_stick = HTTP.get "https://serpapi.com/search.json?q=McDonald%27s&location=Dallas&hl=en&gl=us&source=test&stick=#{stick_parameter}"
         @json_stick = @response_stick.parse
       end
 
@@ -71,17 +64,17 @@ describe "SerpApi Desktop JSON" do
       end
 
       it "has `people_also_search_for for` block" do
-        expect(@json_stick["knowledge_graph"]["top"]["people_also_search_for"]).to be_an(Array)
+        expect(@json_stick["knowledge_graph"]["people_also_search_for"]).to be_an(Array)
       end
 
       it "has more `people_also_search_for` results" do
-        expect(@json_stick["knowledge_graph"]["top"]["people_also_search_for"].size).to be > 3
+        expect(@json_stick["knowledge_graph"]["people_also_search_for"].size).to be > 10
       end
 
       describe "has `people_also_search_for` first result" do
 
         before :all do
-          @first_alternative = @json_stick["knowledge_graph"]["top"]["people_also_search_for"][0]
+          @first_alternative = @json_stick["knowledge_graph"]["people_also_search_for"][0]
         end
 
         it "has a name" do
